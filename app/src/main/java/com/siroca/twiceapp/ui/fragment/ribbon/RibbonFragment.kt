bcf9afbca93648288fe16c04ab43fec46733075f
lezvie <asanalievurmat10@gmail.com>
2022-07-06 16:10:29 +0300

    RecyclerView

--- app/src/main/java/com/siroca/twiceapp/ui/fragment/ribbon/RibbonFragment.kt
+++ app/src/main/java/com/siroca/twiceapp/ui/fragment/ribbon/RibbonFragment.kt
@@ -2,7 +2,7 @@ package com.siroca.twiceapp.ui.fragment.ribbon
 
 import com.example.core.base.BaseFragment
 import com.siroca.twiceapp.databinding.FragmentRibbonBinding
-
+import com.siroca.twiceapp.ui.fragment.ribbon.adapter.RibbonAdapter
 
 class RibbonFragment : BaseFragment<FragmentRibbonBinding>(
     FragmentRibbonBinding::inflate
---
+++ app/src/main/java/com/siroca/twiceapp/ui/fragment/ribbon/adapter/RibbonAdapter.kt
@@ -0,0 +1,25 @@
+package com.siroca.twiceapp.ui.fragment.ribbon.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RibbonAdapter : RecyclerView.Adapter<RibbonAdapter.ViewHolder>() {
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
